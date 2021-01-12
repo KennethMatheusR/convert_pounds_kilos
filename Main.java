c74823164d35bd2f9ffa1e706cd121386357c50c
KENNETH MATHEUS R <kenneth.matheus@gmail.com>
2021-01-12 19:40:12 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+
+    Double pounds = 2d;
+    Double Kilograms = 0.45359237d;
+
+    Double output = pounds * Kilograms;
+
+    System.out.println(pounds + " pounds are " + output + " Kilograms");
+  }
+}
